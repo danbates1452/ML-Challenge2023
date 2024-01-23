62f05d9e86365afc6a08fe4d5363967c1a605fc6
Daniel Bates <42376377+danbates1452@users.noreply.github.com>
2024-01-23 18:30:36 +0000

    still not sure what I'm doing

--- visualiser.py
+++ visualiser.py
@@ -4,7 +4,6 @@ import numpy as np
 
 df=pd.read_csv("./data/movementSensorData.csv")
 
-
 print(df.keys())
 x=df.loc[df['activity'] == 1, 'lw_x']
 y=df.loc[df['activity'] == 4, 'lw_x']
@@ -32,3 +31,24 @@ plt.legend(loc="upper right")
 plt.xlabel("Time step")
 plt.ylabel("Value")
 plt.show()
+
+#import seaborn
+#seaborn.set_style('darkgrid')
+
+#plt.figure(figsize=(5,4))
+#plot_axes = plt.axes(projection = '3d')
+#walkingx = df.loc[df['activity'] == 1, 'lw_x']
+#walkingy = df.loc[df['activity'] == 1, 'lw_y']
+#walkingz = df.loc[df['activity'] == 1, 'lw_z']
+
+#drivingx = df.loc[df['activity'] == 4, 'lw_x']
+#drivingy = df.loc[df['activity'] == 4, 'lw_y']
+#drivingz = df.loc[df['activity'] == 4, 'lw_z']
+
+#plot_axes.scatter3D(walkingx, walkingy, walkingz)
+#plot_axes.scatter3D(drivingx, drivingy, drivingz)
+
+#plot_axes.set_xlabel('x')
+#plot_axes.set_ylabel('y')
+#plot_axes.set_zlabel('z')
+#plt.show()
